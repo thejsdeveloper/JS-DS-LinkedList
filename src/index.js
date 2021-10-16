00b470bb6c2a9d9c4ad627f185603837d3c074a0
vikas yadav <thejsdeveloper@gmail.com>
2021-10-17 01:12:06 +0530

    improving gitignore

--- src/index.js
+++ src/index.js
@@ -2,12 +2,50 @@ import "./styles.css";
 import { LinkedList } from "./linkedlist";
 
 document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
+	<header class="container" id="header">
+    <img src=${require("./assets/images/logo.png")} class="logo"/>
+		<p class="title">LinkedList</p>
+	</header>
+  
+    <div class="container column">
+ 
+     <section class="description mt-2">
+      <h3 >Methods in LinkedList</h3>
+          <hr/>
+      <ul class="list">
+      <li><strong>add(element)</strong>: Takes an element and add it to the list</li>
+      
+      <li><strong>insertAt(element, index)</strong>: Adds the element at the specified index</li>
+      
+      <li><strong>addFirst(element)</strong>: Takes an element and add it to start of the list</li>
+      
+      <li><strong>addLast(element)</strong>: Takes an element and add it to end of the list</li>
+      
+      <li><strong>getAt(index)</strong>: Return the element at the specified index</li>
+      
+      <li><strong>indexOf(element)</strong>: Returns index of the passed the element. If the element do not exist in list it returns -1</li>
+      
+      <li><strong>removeAt(index)</strong>: Removes the element at the specified index and return the removed element</li>
+      
+      <li><strong>remove(element)</strong>: Removes the element if it exist in list and returned the removed element</li>
+      
+      <li><strong>size</strong>: A getter method which return size of the list</li>
+      
+      <li><strong>isEmpty()</strong>: Return true if list is empty otherwise return false</li>
+      
+      <li><strong>clear()</strong> Clears the list</li>
+      
+      <li><strong>toString()</strong> Return the string representation of the list</li>
+	</ul>
+
+ 
+     </section>
+
+     <section class="note">
+       <h2>To view the logs, please Open Console in the browser.</h2>
+     </section>
+    
+     </div>
 `;
 
 const list = new LinkedList();
